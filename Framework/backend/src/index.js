b66246757de9077c85761ef1b32c250f0e0998bb
Jare <aspirebyjare@gmail.com>
2025-10-07 13:45:18 -0700

    Framework proposal for project.

---
+++ Framework/backend/src/index.js
@@ -0,0 +1,33 @@
+// const express = require("express")
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT
+
+app.get('/health', (_req, res) => res.status(200).send('ok'));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
+
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
+
+app.listen(PORT, '0.0.0.0' , () => {
+    console.log("Server is running on PORT:" + PORT);
+    connectDB();
+});
---
+++ Framework/frontend/src/store/useAuthStore.js
@@ -0,0 +1,29 @@
+import { create } from "zustand";
+//import { checkAuth } from "../../../backend/src/controllers/auth.controller.js";
+import { axiosInstance } from "../lib/axios.js";
+
+export const useAuthStore = create((set) =>({
+    authUser: null,
+    isSigningUp: false,
+    isLoggingIn: false,
+    isUpdatingProfile: false,
+
+    isCheckingAuth: true,
+
+
+    checkAuth: async() => {
+        try{
+            const res = await axiosInstance.get("/auth/check");
+
+            set({ authUser:res.data});
+        } catch (error){
+            set({authUser: null});
+        } finally {
+            set({ isCheckingAuth: false});
+        }
+    },
+
+    signup: async (data) => {
+
+    }
+}));
