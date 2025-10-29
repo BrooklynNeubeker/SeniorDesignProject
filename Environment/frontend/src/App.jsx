80cff3c09a253620feac11d46537796e16c93789
Ann Regala <regala3@unlv.nevada.edu>
2025-10-29 14:48:43 -0700

    added the search functionality to the map

--- Environment/frontend/src/App.jsx
+++ Environment/frontend/src/App.jsx
@@ -42,7 +42,7 @@ const App = () => {
 
   return (
     <div data-theme={theme}>
-      <Navbar />
+      {/*<Navbar/>*/}
 
       <Routes>
         <Route
---
+++ Environment/frontend/src/components/Search.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from 'react'
+
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+import { useMap } from 'react-leaflet';
+import 'leaflet-geosearch/assets/css/leaflet.css';
+
+{/* Most of this code is given by the plugin's github
+    More info here: https://github.com/smeijer/leaflet-geosearch
+    Need to edit the navbar to where it is hideable on the site-creation-page
+*/}
+const Search = ({ apiKey }) => {
+  const provider = new OpenStreetMapProvider({
+    params: {
+        email: 'annregalab@gmail.com',
+    },
+  });
+
+  // @ts-ignore
+  const searchControl = new GeoSearchControl({
+  provider: provider, // required
+  showMarker: true, // optional: true|false  - default true
+  showPopup: false, // optional: true|false  - default false
+  marker: {
+    // optional: L.Marker    - default L.Icon.Default
+    icon: new L.Icon.Default(),
+    draggable: false,
+  },
+  style: 'bar',
+  maxMarkers: 1, // optional: number      - default 1
+  retainZoomLevel: false, // optional: true|false  - default false
+  animateZoom: true, // optional: true|false  - default true
+  autoClose: false, // optional: true|false  - default false
+  searchLabel: 'Enter address', // optional: string      - default 'Enter address'
+  keepResult: false, // optional: true|false  - default false
+  updateMap: true, // optional: true|false  - default true
+});
+
+  const map = useMap();
+  useEffect(() => {
+    map.addControl(searchControl);
+    return () => map.removeControl(searchControl);
+  }, []);
+
+  return null;
+};
+
+export default Search;
