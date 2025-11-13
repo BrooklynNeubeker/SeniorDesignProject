c4cbbe71e7934f5b7ec67c13f7e21e970718dff0
Brooklyn Neubeker <neubeker@unlv.nevada.edu>
2025-11-12 17:20:23 -0800

    Map now loads into the right location when refreshing or coming back to work later when state is lost.

--- Environment/frontend/src/pages/SitePlanPage.jsx
+++ Environment/frontend/src/pages/SitePlanPage.jsx
@@ -4,18 +4,17 @@ import Overlay from '../components/Overlay';
 import { useGlobal } from "../components/GlobalContext";
 import DefaultMap from '../components/DefaultMap';
 import {useParams} from "react-router-dom";
+import {Loader2} from "lucide-react";
 import {axiosInstance} from "../lib/axios";
 import { map } from 'leaflet';
 
 const SitePlanPage = () => {
-    const { imperial, location, zoom } = useGlobal();
+    const { imperial, location, setLocation, zoom} = useGlobal();
     const saveBtnRef = useRef();
     const{ id } = useParams();
-    // console.log(location);
     // Keep track of structures added, these will be rendered on map
     const [structures, setStructures] = useState([]);
     const [loading, setLoading] = useState(true);
-
     const [myMap, setMap] = useState([]);
     // const [newMap, setNewMap] = useState([{mapCenter: {x: location.lng, y: location.lat}, eventID: id, zoomLevel: zoom, mapMarkers: []}]);
 
@@ -45,7 +44,16 @@ const SitePlanPage = () => {
                 console.log("Fetched existing map:", res.data);
             }
             setMap(res.data || []);
-
+            const center = res.data[0].mapCenter;
+            // console.log(center.x['$numberDecimal']); //Set the state variable to display the map at the right location!!
+            // console.log(center.y['$numberDecimal']);
+            setLocation({ //Set the location globals, but this won't update them right this moment so it's mostly just for the markers
+                lat: center.y['$numberDecimal'],
+                lng: center.x['$numberDecimal'],
+                label: location.label,
+            });
+            // console.log(location.lat);
+            // console.log(location.lng);
             if (res.data && res.data.length > 0) { setStructures(res.data[0].mapMarkers || []); }
         } catch (error) {
             console.error("Failed to load map:", error);
@@ -63,17 +71,15 @@ const SitePlanPage = () => {
         }
     }, []);
 
-
     const saveEventMap = async (e) => {
         e.preventDefault();
-
+        // var payload = null;
         const payload = {
-            mapCenter: { x: location.lng, y: location.lat },
+            mapCenter: { x: location.lng, y: location.lat }, //IF there was a search, use the new location.lng/location.lat. Dont' ask me why, but setting centerX or Y state using these uses whatever they were when the page loaded, not hte search, so it has to be this way...
             eventID: id,
             zoomLevel: zoom,
             mapMarkers: structures
         }
-
         setMap(prev => [...prev, payload])
 
         try {
@@ -87,7 +93,6 @@ const SitePlanPage = () => {
         let res = await axiosInstance.get(`/events/${id}/site-plan`);
         setMap(res.data)
     }
-
     
     /* var currMap = myMap;
 
@@ -174,7 +179,7 @@ const SitePlanPage = () => {
     const addStructure = (name, Icon, bgColor, iconColor, border, description, tagType) => {
         const newStructure = { id: crypto.randomUUID(),
             name, Icon, bgColor, iconColor, border, description, tagType, orientation: 0,
-            dimensions: [20, 20], position: [location.lat, location.lng]}
+            dimensions: [20, 20], position: [location.lat, location.lng]} //Show structures at the center from db
         setStructures(prev => [...prev, newStructure])
     }
 
@@ -182,11 +187,18 @@ const SitePlanPage = () => {
         setStructures(prev => prev.filter(structure => structure.id !== id));
     }
 
+    while (loading) { //Load until db has been fetched and the global variables are updated
+        return <>
+                  <Loader2 className="h-5 w-5 animate-spin" />
+                  Loading...
+                </>;
+    }
+
     return (
         <div>
             <div className="fixed inset-0 z-10">
                 {/* Some function checking if there is a function to check */}
-                <Map structures={structures} removeStructure={removeStructure} center={[location.lat, location.lng]} 
+                <Map structures={structures} removeStructure={removeStructure} center={[location.lat, location.lng]}  //Map should display at center coordinates from db by default if a search has not happened. To make these place at the right spot immediately after a search, change it back to location.lat and location.lng, but be aware that that means that when the map loads in and no search has happened, 
                 saveBtnRef={saveBtnRef} imperial={imperial}/> 
                
                 {/* Render structures on Map component, pass in structures prop */}
@@ -203,7 +215,6 @@ const SitePlanPage = () => {
                     <span>Save</span>
                 </button>
             </div>
-
         </div>
     );
 };
