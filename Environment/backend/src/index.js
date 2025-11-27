3a51bb420d22fa703f552a513a7312f506b0a4db
Brooklyn Neubeker <neubeker@unlv.nevada.edu>
2025-11-26 19:58:28 -0800

    Copying over Ann's changes for deployment

--- Environment/backend/src/index.js
+++ Environment/backend/src/index.js
@@ -15,22 +15,41 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-
+const FRONTEND_URL = process.env.FRONTEND_URL;
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://localhost:5173',
+
+const allowedOrigins = [
+  'http://localhost:5173', // development
+  'https://accessiblemap.org', //Production
+  'https://www.accessiblemap.org' // production with www
+];
+
+const corsOption = {
+  origin: function(origin, callback) {
+    if(!origin) return callback(null, true);
+
+    if(allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }, 
   credentials: true,
-}));
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+};
 
+app.use(cors(corsOption));
 
+console.log(FRONTEND_URL);
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/events", eventRoutes); 
 
 server.listen(PORT, () => {
-  console.log("server is running on PORT:" + PORT);
+  console.log("server is running on:" + PORT);
   connectDB();
 });
--- Environment/frontend/src/lib/axios.js
+++ Environment/frontend/src/lib/axios.js
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+const BASE_URL = import.meta.env.VITE_BASE_BACKEND_URL;
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5001/api",
+    baseURL: BASE_URL + "/api",
     withCredentials: true,
 });
 
+export default axiosInstance;
+console.log(BASE_URL + "/api");
 
