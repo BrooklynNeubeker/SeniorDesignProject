90cc991038c082a21b1170a70fb96129350d30b6
regala3 <annregalab@gmail.com>
2025-11-28 20:17:38 -0800

    reverted back

--- Environment/backend/src/index.js
+++ Environment/backend/src/index.js
@@ -31,20 +31,11 @@ const corsOption = {
   origin: function(origin, callback) {
     if(!origin) return callback(null, true);
 
-    // if(allowedOrigins.indexOf(origin) !== -1) {
-    //   callback(null, true);
-    // } else {
-    //   callback(new Error('Not allowed by CORS'));
-    // }
-
-    const url = origin.replace(/\/$/, "");
-
-    if(allowedOrigins.includes(url)) {
+    if(allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
     }
-
   }, 
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
